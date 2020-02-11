99f9f41594701fe850f11b76ecbc8863514cc30e
jiama <dearwinter@foxmail.com>
2020-02-11 10:44:51 +0800

    code style

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const http = require('http');
 const chalk = require('chalk');
+const path = require('path');
 const defaultConfig = require('./config/defaultConfig');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<body>');
+	const url = req.url;
+	const filePath = path.join(defaultConfig.root, url);
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<body>');
 
-  res.write('hello11111 nodejs')
-  res.write('<body/>');
-  res.end('<html/>');
+	res.write(filePath);
+	res.write('<body/>');
+	res.end('<html/>');
 });
 
 server.listen(defaultConfig.port, defaultConfig.hostname, () => {
-  console.log(chalk.green(`Server running at http://${defaultConfig.hostname}:${defaultConfig.port}/`));
+	console.log(
+		chalk.green(
+			`Server running at http://${defaultConfig.hostname}:${defaultConfig.port}/`
+		)
+	);
 });
