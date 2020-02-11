8635b2961e59fd9195f7b8783207b962d23c6565
jiama <dearwinter@foxmail.com>
2020-02-11 10:07:03 +0800

    分享config、支持chalk

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-// const http = require('http');
+const http = require('http');
+const chalk = require('chalk');
+const defaultConfig = require('./config/defaultConfig');
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
 
-
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
+server.listen(defaultConfig.port, defaultConfig.hostname, () => {
+  console.log(chalk.green(`Server running at http://${defaultConfig.hostname}:${defaultConfig.port}/`));
+});
