5a6cd7aad3fb9de003c57b6d1b07fa434c1385f5
jiama <dearwinter@foxmail.com>
2020-02-11 11:04:11 +0800

    创建可读流

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const http = require('http');
 const chalk = require('chalk');
 const path = require('path');
+const fs = require('fs');
 const defaultConfig = require('./config/defaultConfig');
 
 const server = http.createServer((req, res) => {
 	const url = req.url;
 	const filePath = path.join(defaultConfig.root, url);
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
+    fs.stat(filePath, (err, stats) => {
+        if (err) {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(404);
+            return;
+        }
+        res.statusCode = 200;
+        if (stats.isFile()) {
+            res.setHeader('Content-Type', 'text/plain');
+            fs.createReadStream(filePath).pipe(res); // 用createStream比readFile好
+        } else if () {
+
+        }
+    });
 	res.write('<html>');
 	res.write('<body>');
 
